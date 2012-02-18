1d4549ecc8f9ab8721a7a2f3004d6a3eede3a380
Pablo Villalba <pablo@teambox.com>
2012-02-19 00:29:39 +0100

    Refactoring JS

--- game.js
+++ game.js
@@ -1,100 +1,73 @@
-var canvas = document.getElementById('canvas')
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+var GLGE = window.GLGE,
+    canvas = document.getElementById('canvas'),
+    XMLdoc = new GLGE.Document(),
+    app;
 
-var renderer = new GLGE.Renderer(canvas);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-var XMLdoc = new GLGE.Document();
-var camera;
-var cameraOffset;
+app = {
+  init: function () {
+    // Initialize modules
+    this.renderer = new GLGE.Renderer(canvas);
+    this.keys = new GLGE.KeyInput();
 
-var player, wheel1, wheel2, car, scene;
+    // Get elements
+    this.scene = XMLdoc.getElement("mainscene");
+    this.camera = XMLdoc.getElement("mainCamera");
+    this.car = XMLdoc.getElement("car");
+    this.player = XMLdoc.getElement("player");
 
+    // Configure world settings
+    this.scene.setGravity([0, -9.8, 0, 0]);
+    this.renderer.setScene(this.scene);
+    this.renderer.render();
 
-XMLdoc.onLoad = function () {
-  scene = XMLdoc.getElement("mainscene");
-  camera = XMLdoc.getElement("mainCamera");
-  cameraOffset = XMLdoc.getElement("cameraOffset");
-  car = XMLdoc.getElement("car");
-  player = XMLdoc.getElement("player");
-  wheel1 = XMLdoc.getElement("wheel1");
-  wheel2 = XMLdoc.getElement("wheel2");
+    // Start rendering
+    this.render.call(this);
+  },
+  render: function () {
+    var now = +new Date(),
+        rot, speed, dt, speed_x, speed_z;
 
-  var keys = new GLGE.KeyInput();
-
-  /*
-  var positions=[];
-  for(var x=-50; x<50;x++){
-    if(x!=0){
-      positions.push(x); positions.push(0); positions.push(-50); positions.push(x);
-      positions.push(0); positions.push(50); positions.push(50); positions.push(0);
-      positions.push(x); positions.push(-50); positions.push(0); positions.push(x);
-    }
-  }
-  
-  var line=(new GLGE.Object).setDrawType(GLGE.DRAW_LINES);
-  line.setMesh((new GLGE.Mesh).setPositions(positions));
-  line.setMaterial(XMLdoc.getElement( "lines" ));
-  scene.addObject(line);
-  */
-    
-  scene.setGravity([0, -9.8, 0, 0]);
-  renderer.setScene(scene);
-  renderer.render();
-  var lasttime;
-  
-  
-  
-  var rot = 0;
-  var speed = 0;
-  var render = function (){
-    var now = +new Date;
     // Player physics
-    if (keys.isKeyPressed(GLGE.KI_W)) {	
+    if (this.keys.isKeyPressed(GLGE.KI_W)) {
       speed = 6;
-    } else if (keys.isKeyPressed(GLGE.KI_S)) {
+    } else if (this.keys.isKeyPressed(GLGE.KI_S)) {
       speed = -6;
     } else {
       speed = 0;
     }
-    if (keys.isKeyPressed(GLGE.KI_SPACE)) {
-      if (player.locY < 1) {
-        //player.setVelocityY(10);
-        speed = 0;
-      }
-
-
-
-
+    if (this.keys.isKeyPressed(GLGE.KI_SPACE)) {
+      console.log('jump!');
     }
-    if(keys.isKeyPressed(GLGE.KI_A)) {
+    if (this.keys.isKeyPressed(GLGE.KI_A)) {
       rot = 2;
-    } else if(keys.isKeyPressed(GLGE.KI_D)) {
+    } else if (this.keys.isKeyPressed(GLGE.KI_D)) {
       rot = -2;
     } else {
       rot = 0;
     }
 
-    if (lasttime) {
-      var dt = (now - lasttime) / 500;
-      var speed_x, speed_y;
-      scene.physicsTick(dt);
-      if (player.locY < 1) {
-        player.setRotY(player.rotY + rot*dt);
-        speed_x = speed * Math.cos(player.rotY);
-        speed_z = speed * Math.sin(-player.rotY);
-        player.setLocX(player.locX + speed_x*dt);
-        player.setLocZ(player.locZ + speed_z*dt);
-        camera.setLocX(player.locX);
-        camera.setLocZ(player.locZ+40);
+    if (this.lasttime) {
+      dt = (now - this.lasttime) / 500;
+      this.scene.physicsTick(dt);
+      if (this.player.locY < 1) {
+        this.player.setRotY(this.player.rotY + rot * dt);
+        speed_x = speed * Math.cos(this.player.rotY);
+        speed_z = speed * Math.sin(-this.player.rotY);
+        this.player.setLocX(this.player.locX + speed_x * dt);
+        this.player.setLocZ(this.player.locZ + speed_z * dt);
+        this.camera.setLocX(this.player.locX);
+        this.camera.setLocZ(this.player.locZ + 40);
       }
     }
-    lasttime = now;
-    renderer.render();
-    window.requestAnimationFrame(render);
-  };
-  render();
+    this.lasttime = now;
+    this.renderer.render();
+    window.requestAnimationFrame(this.render.bind(this));
+  }
 };
 
+XMLdoc.onLoad = app.init.bind(app);
 XMLdoc.load("scene.xml");
 
